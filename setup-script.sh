02397d369173d28cad8b1b7fd0d5ea52e42bb3b4
Justus Magin <keewis@posteo.de>
2026-08-09 18:03:53 +0200

    add a setup script

---
+++ setup-script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# change the username to jovyan
+usermod --login jovyan ubuntu
+groupmod --new-name jovyan ubuntu
+rm -rf /home/jovyan
+usermod --shell /bin/bash --move-home --home /home/jovyan jovyan
+chown -R jovyan:jovyan /srv /home/jovyan
+rm -rf /home/ubuntu
+
+# install some more packages
+apt update -y
+apt install -y emacs-nox git tree ripgrep
