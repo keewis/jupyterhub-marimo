655aafe0de721bd138734358ffc25706590dbe1d
Justus Magin <keewis@posteo.de>
2026-08-09 18:40:00 +0200

    create and allow modifying the singleuser log

--- setup-script.sh
+++ setup-script.sh
@@ -8,6 +8,9 @@ usermod --shell /bin/bash --move-home --home /home/jovyan jovyan
 chown -R jovyan:jovyan /srv /home/jovyan
 rm -rf /home/ubuntu
 
+touch /var/log/jupyterhub-singleuser.{err,log}
+chmod +w /var/log/jupyterhub-singleuser.{err,log}
+
 # install some more packages
 apt update -y
 apt install -y emacs-nox git tree ripgrep
